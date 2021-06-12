2be7aca21e47beb5220baf28fb39940374a62453
bu4er88 <bu4er88@gmail.com>
2021-06-12 23:23:20 +0300

    Fixed get() method

--- Singly_Linked_List.py
+++ Singly_Linked_List.py
@@ -8,7 +8,7 @@ class Node:
 class MyLinkedList():
 
     def __init__(self):
-        """
+        """"
         Initialize your data structure here.
         """
         self.head = None
@@ -37,7 +37,7 @@ class MyLinkedList():
         Get the value of the index-th node in the linked list.
         If the index is invalid, return -1
         """
-        if self.head is None or index > len(self):
+        if self.head is None or index >= len(self):
              return -1
         count = 0
         node = self.head
@@ -58,12 +58,12 @@ class MyLinkedList():
     def addAtTail(self, val: int) -> None:
         """Append a node of value val to the last element of the linked list."""
         if self.head is None:
-            self.head = Node(val)  # next=None
+            self.head = Node(val)   # next=None
+            return
         node = self.head
         while node.next:
             node = node.next
-        print(node.val)
-        node.next = Node(val)  # next=None
+        node.next = Node(val)       # next=None
 
     def addAtIndex(self, index: int, val: int) -> None:
         """
@@ -74,9 +74,9 @@ class MyLinkedList():
         if index < 0 or index > len(self):
             raise IndexError('Index is not valid!')
         if index == 0 or self.head is None:
-            self.addAtHead(val)  # next=None
+            self.addAtHead(val)     # next=None
         if index == len(self):
-            self.addAtTail(val)
+            self.addAtTail(val)     # next=None
         count = 0
         node = self.head
         while node:
@@ -113,19 +113,25 @@ class MyLinkedList():
 # obj.deleteAtIndex(index)
 
 myLinkedList = MyLinkedList()
-myLinkedList.addAtHead(3)
-print('llist:', myLinkedList)
-myLinkedList.addAtHead(2)
-print('llist:', myLinkedList)
-myLinkedList.addAtHead(1)
-print('llist:', myLinkedList)
-myLinkedList.addAtTail(0)
-print('llist:', myLinkedList)
-myLinkedList.addAtIndex(2, "I'm here")  # linked list becomes 1->2->3
-print('llist:', myLinkedList)
-print(myLinkedList.get(0))              # return 2
-myLinkedList.deleteAtIndex(1)        # now the linked list is 1->3
-print('llist:', myLinkedList)
-print(myLinkedList.get(1))                  # return 3
-print('llist:', myLinkedList)
-print('Length:', len(myLinkedList))
+#print(myLinkedList, len(myLinkedList))
+myLinkedList.addAtHead('head')
+# # print(myLinkedList, len(myLinkedList))
+myLinkedList.addAtHead('second_head')
+# # print(myLinkedList, len(myLinkedList))
+myLinkedList.addAtHead('third head')
+# # print(myLinkedList, len(myLinkedList))
+myLinkedList.addAtTail('tail')
+print(myLinkedList, len(myLinkedList))
+# print('llist:', myLinkedList)
+myLinkedList.addAtIndex(2, "New 2nd node")  # linked list becomes 1->2->3
+print(myLinkedList, len(myLinkedList))
+# print(myLinkedList.get(0))              # return 2
+myLinkedList.deleteAtIndex(0)        # now the linked list is 1->3
+print(myLinkedList, len(myLinkedList))
+# print(myLinkedList.get(1))                  # return 3
+# print('llist:', myLinkedList)
+# print('Length:', len(myLinkedList))
+print(myLinkedList.get(1))
+myLinkedList.addAtTail('second_tail')
+print(myLinkedList, myLinkedList.get(4))
+
