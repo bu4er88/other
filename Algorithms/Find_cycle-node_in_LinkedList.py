11bee8a325a1dd3d620292ff170edb81ba2b524a
bu4er88 <bu4er88@gmail.com>
2021-06-15 23:53:57 +0300

    Solution works

--- Algorithms/Find_cycle-node_in_LinkedList.py
+++ Algorithms/Find_cycle-node_in_LinkedList.py
@@ -1,13 +1,17 @@
+'''
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect.
+If the two linked lists have no intersection at all, return null.
+'''
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-
 class Solution:
-    def detectCycle(self, head):
-        # Solution 1 --> O(n)
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        # Solution 1 --> O(n) memory and O(n) speed
         # seen = set()
         # i = head
         # j = head.next
@@ -20,16 +24,16 @@ class Solution:
         #     count += 1
         #     head = head.next
 
-        # Solution 2 --> O(1)
-        i = head
-        j = head
-        while j != None and j.next != None:
-            i = i.next
-            j = j.next.next
-            if i == j:
-                i = head
-                while i != j:
-                    i = i.next
-                    j = j.next
-                return i
-        return None
+        # Solution 2 --> O(1) memory and O(n) speed
+        i = headA
+        j = headB
+        while i != j:
+            if i is None:
+                i = headB
+            else:
+                i = i.next
+            if j is None:
+                j = headA
+            else:
+                j = j.next
+        return i
